b66e649ef749216194fe36e828315398dcf95a48
9junko9 <jennifer.rivolier@gmail.com>
2024-06-09 22:17:12 +0200

    modif style nav et ajout page login

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -24,3 +24,44 @@ async function displayWorks() {
   });
 }
 displayWorks();
+
+// Fonction: masque les figures non demandés (filtre) en activant le "display:none"
+function filterWorksToGallery(category) {
+  const figures = document.querySelectorAll(".gallery figure");
+  for (i = 0; i < figures.length; i++) {
+    if (figures[i].className.includes(category)) {
+      figures[i].classList.remove("figureHidden");
+    } else {
+      figures[i].classList.add("figureHidden");
+    }
+  }
+}
+
+// Barre de filtres, lorsque on clique sur le bouton de filtre => appeler la fonction qui va gerer les figures + CSS des boutons
+const filterButtons = document.querySelectorAll(".filterGallery-btn");
+filterButtons[0].classList.add("btn-active");
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    filterButtons.forEach((btn) => btn.classList.remove("btn-active"));
+
+    switch (button.className) {
+      case "filterGallery-btn filterAll":
+        filterWorksToGallery("");
+        break;
+      case "filterGallery-btn filterObjects":
+        filterWorksToGallery("categoryId1");
+        break;
+      case "filterGallery-btn fitlerApartments":
+        filterWorksToGallery("categoryId2");
+        break;
+      case "filterGallery-btn filterHotelsRestaurants":
+        filterWorksToGallery("categoryId3");
+        break;
+      default:
+        console.error("Ce bouton ne correspond à aucun case (switch).");
+    }
+    button.classList.add("btn-active");
+  });
+});
